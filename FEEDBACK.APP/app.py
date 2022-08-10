2708dec18cef2a5ce497e022ecf07c8c45f21759
Hector Lopez <hectorlopez.view@gmail.com>
2022-08-09 19:30:57 -0700

    Implement db connection and create db model.

--- FEEDBACK.APP/app.py
+++ FEEDBACK.APP/app.py
@@ -1,6 +1,35 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+ENV = 'dev'
+    
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:impala@localhost/buick'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer,primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
 
 @app.route('/')
 def index():
@@ -19,5 +48,4 @@ def submit():
         return render_template('success.html')
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
